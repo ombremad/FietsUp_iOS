c325cb62eb8aec70e4897ea816bdaf3e1426d0a9
Anne Ferret <anne@anneferret.eu>
2026-05-04 10:33:34 +0200

    Initial Commit

---
+++ FietsUp_iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FietsUp_iOS
+//
+//  Created by Anne Ferret on 04/05/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FietsUp_iOS/FietsUp_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FietsUp_iOSApp.swift
+//  FietsUp_iOS
+//
+//  Created by Anne Ferret on 04/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct FietsUp_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
